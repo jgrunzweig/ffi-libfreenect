b2526bfcbc172adff2de9e5fa0982d44d1e5d63a
Eric Monti <esmonti@gmail.com>
2010-12-19 16:27:57 -0600

    added depth/video format and size helpers

--- lib/freenect.rb
+++ lib/freenect.rb
@@ -9,4 +9,32 @@ module Freenect
   def self.init(*args)
     Context.new(*args)
   end
+
+  def self.lookup_video_format(fmt)
+    return (fmt.is_a?(Numeric) ? fmt : FFI::Freenect::VIDEO_FORMATS[fmt])
+  end
+
+  def self.lookup_video_size(fmt)
+    l_fmt = (fmt.is_a?(Numeric) ? FFI::Freenect::VIDEO_FORMATS[fmt] : fmt)
+    if l_fmt.nil? or (sz = FFI::Freenect::VIDEO_SIZES[l_fmt]).nil?
+      return nil
+    else
+      return sz
+    end
+  end
+
+
+  def self.lookup_depth_format(fmt)
+    return (fmt.is_a?(Numeric) ? fmt : FFI::Freenect::DEPTH_FORMATS[fmt])
+  end
+
+  def self.lookup_depth_size(fmt)
+    l_fmt = (fmt.is_a?(Numeric) ? FFI::Freenect::DEPTH_FORMATS[fmt] : fmt)
+    if l_fmt.nil? or (sz = FFI::Freenect::DEPTH_SIZES[l_fmt]).nil?
+      return nil
+    else
+      return sz
+    end
+  end
+
 end
